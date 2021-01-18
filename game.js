00033aabf73117e19ccbcf6176230c5a0308b86b
ToyBotDroid <nischan127@gmail.com>
2021-01-18 17:32:26 +0600

    finally works

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ let character = document.querySelector('.character')
 let block = document.querySelector('.block')
 let screen = document.querySelector('.screen')
 let displayScore = document.querySelector('.display-score')
+let restartBtn = document.querySelector('.restart')
+
 
 let score = 0;
 const scoreIncrement = setInterval(()=>{
@@ -21,7 +23,7 @@ screen.addEventListener('click', () => {
   jump()
 })
 
-//spacebar event = keyup or keydown
+
 document.addEventListener('keydown', event => {
   if (event.code === 'Space') {
     jump()
@@ -33,20 +35,16 @@ let detectContact = setInterval(()=>{
 let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'))
 let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('Left'))
 
-if( blockLeft === 50 ){
-  alert('you lost')
+if( blockLeft >= 50 && blockLeft <= 100 && characterTop >= 250){
+  alert(`You hit the block. You lost \n Your Score : ${score}`)
+  block.classList.remove('move')
+  clearInterval(scoreIncrement)
 }
+}, 10)
 
-}, 1)
+restartBtn.addEventListener('click', ()=> {
+  location.reload()
+})
 
 
- // clearInterval(scoreIncrement)
 
-/*
-to make it responsive
-
-1. vh and vw can be implemented. And for checking the  position of the character and block has to be compared assigning a variable each for the window.getComputedStyle(character).getPropertyValue('top') values
-
-2. or make double the code, which i firmly believe isn't worthy or the right way to code 
-
-*/
